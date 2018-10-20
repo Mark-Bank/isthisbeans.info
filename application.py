3961fccb93d2aafca2586125202c24233cf5d7d6
mark.bank <mark.bank@ibm.com>
2018-10-20 17:23:58 -0400

    put out some beans

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import logging.handlers
 
@@ -22,103 +23,17 @@ handler.setFormatter(formatter)
 # add Handler to Logger
 logger.addHandler(handler)
 
-welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+api_key = os.environ['visual_recognition_api_key']
+api_endpoint = os.environ['visual_recognition_base_endpoint']
 
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 
-        http://aws.Amazon/apache2.0/
 
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Welcome</title>
-  <style>
-  body {
-    color: #ffffff;
-    background-color: #E0E0E0;
-    font-family: Arial, sans-serif;
-    font-size:14px;
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: none;
-  }
-  body.blurry {
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: #fff 0px 0px 25px;
-  }
-  a {
-    color: #0188cc;
-  }
-  .textColumn, .linksColumn {
-    padding: 2em;
-  }
-  .textColumn {
-    position: absolute;
-    top: 0px;
-    right: 50%;
-    bottom: 0px;
-    left: 0px;
-
-    text-align: right;
-    padding-top: 11em;
-    background-color: #1BA86D;
-    background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
-    background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
-  }
-  .textColumn p {
-    width: 75%;
-    float:right;
-  }
-  .linksColumn {
-    position: absolute;
-    top:0px;
-    right: 0px;
-    bottom: 0px;
-    left: 50%;
-
-    background-color: #E0E0E0;
-  }
-
-  h1 {
-    font-size: 500%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  h2 {
-    font-size: 200%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  ul {
-    padding-left: 1em;
-    margin: 0px;
-  }
-  li {
-    margin: 1em 0em;
-  }
-  </style>
-</head>
-<body id="sample">
-  <div class="textColumn">
-    <h1>Smello, world</h1>
-    <p>Your second AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
-  </div>
-  
-</body>
-</html>
-"""
+#curl -X POST -u "apikey:${api_key}" \
+#--form "images_file=@${images_dir}/beans.jpg" \
+#"https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?version=2018-03-19"
 
 def application(environ, start_response):
+    logger.info("WOO! api_endpoint: %s" % api_endpoint)
     path    = environ['PATH_INFO']
     method  = environ['REQUEST_METHOD']
     if method == 'POST':
@@ -135,7 +50,9 @@ def application(environ, start_response):
         response = ''
     else:
         logger.info("... you're a real go-getter, eh? >:)")
-        response = welcome
+        # TODO use a real framework at some point, eh
+        with open('static/index.html', 'r') as my_index:
+            response = my_index.read()
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
 
